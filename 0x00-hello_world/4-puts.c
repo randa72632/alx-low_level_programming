443b5201cd974b60be81e34843af6a7ea1929f7f
randa72632 <randa.72632@gmail.com>
2022-10-17 05:38:31 -0700

    my first line of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /*
  * main - print a line using puts
  *
- * Return:0
- */
-int main (void)
+ * Return: 0
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
