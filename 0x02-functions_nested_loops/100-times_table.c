c82e08ef6d9a2243c8d72d7517a29af61155c7aa
randa72632 <randa.72632@gmail.com>
2022-10-19 18:51:36 +0200

    print times table advance 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,6 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			
 			_putchar('0');
 
 			for (mult = 1; mult <= n; mult++)
@@ -30,7 +29,7 @@ void print_times_table(int n)
 
 				if (prod >= 100)
 				{
-					_putchar((prod / 100) + 'a');
+					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
