dadc9ca740a0345240002b06446bad42c04497c9
randa72632 <randa.72632@gmail.com>
2022-10-19 17:10:07 +0200

    count to 98 task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,9 +11,10 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d, ", n--);
+			printf("%d, ", n--);												
 		printf("%d\n", n);
-	}																																					else 
+	}
+	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
