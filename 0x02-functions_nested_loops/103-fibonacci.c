888cad21e3d330357ed505f3037cd25eedfcd462
randa72632 <randa.72632@gmail.com>
2022-10-22 21:28:13 +0200

    advanced task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,9 +2,9 @@
 
 /**
  * main - prints the sum of even-valued fibonacci sequence											
- *        terms not exceeding 4000000.
+ * terms not exceeding 4000000.
  *
- * Return: Always 0,
+ * Return: Always 0.
  */
 int main(void)
 {
