ee142c8db884313e45588ffcec73303df4d47889
randa72632 <randa.72632@gmail.com>
2022-12-16 11:16:22 +0200

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 gcc -Wall -Wextra -Werror -pedantic -c *.c
-ar rc liball.a *.o
-=======
-gcc -c *.c
-ar rc liball.a *.o
-ranlib liball.a
->>>>>>> 7c73875b607e9260331dcbe290fed313fa7efba1
+ar rc liball.a *o
